0233b6e16dac02dcd8a9251d1748cf5e4464837c
andres <aegifer0806@g.educaand.es>
2024-11-08 21:59:39 +0100

    08/11

--- pmdmt3listview/app/src/main/java/iesmm/pmdm/pmdm_t3_listview/MainActivity.java
+++ pmdmt3listview/app/src/main/java/iesmm/pmdm/pmdm_t3_listview/MainActivity.java
@@ -2,8 +2,11 @@ package iesmm.pmdm.pmdm_t3_listview;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -40,11 +43,32 @@ public class MainActivity extends AppCompatActivity {
         adaptador=new ArrayAdapter(this, android.R.layout.simple_list_item_1, datos);
         lista.setAdapter(adaptador);
 
+        //3. Asociar un evento a cada item
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String cadena="Posición: "+i+
+                        "\nNº Total del items: "+adapterView.getCount()+
+                        "\n Item: "+adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(getApplicationContext(), cadena,Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
     public void putItem(View view){
+        //Localizar el texto del edittex
+        EditText text=this.findViewById(R.id.editText);
 
+        if (!text.getText().toString().isEmpty()){
+            adaptador.add(text.getText().toString());
+        }
+        else {
+            Toast.makeText(this,"No hay nada que insertar", Toast.LENGTH_SHORT).show();
+        }
     }
     public void clearItems(View view){
-
+        if (adaptador!=null){
+            adaptador.clear();
+        }
     }
 }
