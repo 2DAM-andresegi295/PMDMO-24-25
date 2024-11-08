c102a657dac72c7faa695f4c8c5bb0af886559db
andres <aegifer0806@g.educaand.es>
2024-11-08 20:12:19 +0100

    08/11

--- pmdmt3access/app/src/main/java/iesmm/pmdm/pmdm_t3_access/MainActivity.java
+++ pmdmt3access/app/src/main/java/iesmm/pmdm/pmdm_t3_access/MainActivity.java
@@ -1,24 +1,42 @@
 package iesmm.pmdm.pmdm_t3_access;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        addTextView(LocalDate.now().toString());
+        addTextView("Hola Mundo");
+        addTextView(LocalDateTime.now().toString());
+    }
+
+    private void addTextView(String cad){
+        //1. Localizar el layout para agregarle el componente
+        LinearLayout layout = this.findViewById(R.id.container);
+
+        //2. Personalizar TetView
+        TextView box=new TextView(this);
+        box.setText(cad);
+        box.setGravity(Gravity.CENTER);
+        box.setBackgroundColor(Color.GREEN);
+        box.setPadding(10,20,10,20);
+
+        //3. Agregar TextView al layout
+        layout.addView(box);
     }
 }
